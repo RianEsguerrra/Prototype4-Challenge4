02572ce098b510927edc77003a2e0f8eeb97cf92
RianEsguerrra <60616629+RianEsguerrra@users.noreply.github.com>
2021-05-20 13:43:03 -0400

    Challenge Complete V1

---
+++ Prototype 4/Assets/Challenge 4/Scripts/RotateCameraX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateCameraX : MonoBehaviour
+{
+    private float speed = 200;
+    public GameObject player;
+    public float horizontalInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
+
+        transform.position = player.transform.position; // Move focal point with player
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        horizontalInput = movementValue.Get<Vector2>().x;
+    }
+}
---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class RotateCamera : MonoBehaviour
+{
+    private float speed = 20;
+    public float rotationSpeed;
+    public float horizontalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        horizontalInput = movementValue.Get<Vector2>().x;
+    }
+}
